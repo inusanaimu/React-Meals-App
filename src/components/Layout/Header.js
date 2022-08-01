c51caa3299b23a289392bd0e262274e52284b218
inusa naimu <inusanaimu@gmail.com>
2022-08-01 19:43:18 +0100

    Button Cart Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
+import HeaderCartButton from './HeaderCartButton'
 import mealsImage from '../../asserts/meals.jpg'
 import classess from './Header.module.css'
+
 function Header(props) {
   return (
     <>
       <header className={classess.header}>
-        <h1>React Meals</h1>
-        <button>Cart</button>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
       </header>
       <div className={classess['main-image']}>
         <img src={mealsImage} alt='A table full of delicioud foods' />
