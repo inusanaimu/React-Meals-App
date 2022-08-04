ebd6e3534ba0d8d1269fe2df30ade81a4792eb25
inusa naimu <inusanaimu@gmail.com>
2022-08-04 17:00:13 +0100

    Adding Cart Context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 import Cart from './components/Cart/Cart'
+import CartProvider from './Store/CartProvider'
 
 function App() {
   const [cartIsShown, setCartIsShow] = useState(false)
@@ -12,13 +13,13 @@ function App() {
     setCartIsShow(!cartIsShown)
   }
   return (
-    <div>
+    <CartProvider>
       {cartIsShown && <Cart onToggleCart={toggleCart} />}
       <Header onToggleCart={toggleCart} />
       <main>
         <Meals />
       </main>
-    </div>
+    </CartProvider>
   )
 }
 
